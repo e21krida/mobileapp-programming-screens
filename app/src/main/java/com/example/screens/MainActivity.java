42d05f43462d7e3f4fe830dc906d895b9189d61f
e21krida <e21krida@student.his.se>
2022-04-26 09:15:05 +0200

    generated "onPostResume", "onStart", "onStop" & "onDestroy" in both activities and added logcat messages for debugging purposes

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>", "MainActivity onPostResume ran");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "MainActivity onStart ran");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "MainActivity onStop ran");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "MainActivity onDestroy ran");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("==>", "MainActivity onPostResume ran");
     }
 }
--- app/src/main/java/com/example/screens/SecondaryActivity.java
+++ app/src/main/java/com/example/screens/SecondaryActivity.java
@@ -3,12 +3,37 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondaryActivity extends AppCompatActivity {
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>", "SecondaryActivity onPostResume ran");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "SecondaryActivity onStart ran");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "SecondaryActivity onStop ran");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "SecondaryActivity onDestroy ran");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+        Log.d("==>", "SecondaryActivity onCreate ran");
     }
 }
